905a9e2cac6df5e7aca9d35d85831b50de593d25
Samer MACHARA <smachara@gmail.com>
2025-10-03 10:49:30 +0200

    new cv smachara

--- README.md
+++ README.md
@@ -18,3 +18,4 @@ View your app in AI Studio: https://ai.studio/apps/drive/19gR20djd669G9sX2E4yp5J
 2. Set the `GEMINI_API_KEY` in [.env.local](.env.local) to your Gemini API key
 3. Run the app:
    `npm run dev`
+---
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // IMPORTANTE: Cambia 'YOUR_REPO_NAME' por el nombre de tu repositorio en GitHub.
+  // Por ejemplo, si tu repositorio se llama 'cv-react', la línea debería ser:
+  // base: '/cv-react/',
+  base: '/samer-machara-marquez/', 
+})
